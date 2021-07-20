fd5c97652ebf61f14b199162f38d16e046893dcf
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-07-20 14:29:01 -0300

    cookies

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   req.session.id = Math.random().toString().slice(-5);
+  res.cookie('name', 'frg');
   res.render('index', { title: 'Express' });
 });
 
 /* GET home page. */
 router.get('/whoami', function(req, res, next) {
-  res.render('index', { title: 'Express', id: req.session.id });
+  const { cookies }  = req;
+  res.render('index', { title: 'Express', id: req.session.id, cookies});
 });
 
 module.exports = router;
